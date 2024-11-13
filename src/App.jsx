80bb692b2e1d8905ecd4d538288eb06ce7f63483
IulianBotnari <iulianbotnari93@gmail.com>
2024-11-13 14:02:41 +0100

    Add posts in page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import AppHeader from './components/AppHeader/AppHeader'
 import AppMain from "./components/AppMain/AppMain"
 import AppFooter from "./components/AppFooter/AppFooter"
+import Posts from "./data/Posts"
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
   return (
     <>
       <AppHeader />
-      <AppMain />
+      {Posts.map(Posts => <AppMain key={Posts.id} image={Posts.image} title={Posts.title} content={Posts.content} tags={Posts.tags.join(", ")} />)}
       <AppFooter />
     </>
   )
--- src/components/AppMain/AppMain.jsx
+++ src/components/AppMain/AppMain.jsx
@@ -1,10 +1,10 @@
-import AppImg from "../Img/AppImg"
+
 import MainStyle from "./AppMain.module.css"
 import CardStyle from "./AppCard.module.css"
 import AppButton from "../AppButton/AppButton"
 
 
-export default function () {
+export default function (props) {
     //logic here
 
 
@@ -15,11 +15,14 @@ export default function () {
         <>
             <main className={MainStyle.main_el}>
                 <div className={CardStyle.card_el}>
-                    <AppImg />
+                    <img src={props.image} alt="Immagine" />
                     <div className="description">
-                        <h3>Titolo del post</h3>
+                        <h3>{props.title}</h3>
                         <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            {props.content}
+                        </p>
+                        <p>
+                            {props.tags}
                         </p>
 
                         <AppButton />
